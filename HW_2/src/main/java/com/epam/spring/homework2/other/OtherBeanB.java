dcd469700e3948cec7068a782ed8b7297ce9d3ed
Svitlana Zakora <sveta.zakora@gmail.com>
2021-11-09 18:50:59 +0200

    final commit

--- HW_2/src/main/java/com/epam/spring/homework2/other/OtherBeanB.java
+++ HW_2/src/main/java/com/epam/spring/homework2/other/OtherBeanB.java
@@ -1,6 +1,5 @@
 package com.epam.spring.homework2.other;
 
-import com.epam.spring.homework2.beans.BeanA;
 import com.epam.spring.homework2.beans.BeanB;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -13,6 +12,6 @@ public class OtherBeanB {
     @Autowired
     public void setBeanB(BeanB beanB) {
         this.beanB = beanB;
-        System.out.println(this.getClass().getSimpleName() + ". " + beanB.getClass().getSimpleName() + " was injected through the constructor");
+        System.out.println(this.getClass().getSimpleName() + ". " + beanB.getClass().getSimpleName() + " was injected through the setter");
     }
 }
