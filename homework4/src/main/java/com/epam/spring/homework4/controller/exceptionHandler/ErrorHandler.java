ae05967f7be407fd7e8270c6666cd03d883efd41
Svitlana Zakora <sveta.zakora@gmail.com>
2021-11-23 22:23:54 +0200

    HW_6 commit

--- homework4/src/main/java/com/epam/spring/homework4/controller/exceptionHandler/ErrorHandler.java
+++ homework4/src/main/java/com/epam/spring/homework4/controller/exceptionHandler/ErrorHandler.java
@@ -4,18 +4,11 @@ package com.epam.spring.homework4.controller.exceptionHandler;
 import com.epam.spring.homework4.exceptions.DublicateEntryException;
 import com.epam.spring.homework4.exceptions.NotFoundException;
 import lombok.extern.slf4j.Slf4j;
-import org.hibernate.exception.ConstraintViolationException;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
-import java.sql.SQLException;
-import java.sql.SQLIntegrityConstraintViolationException;
 
 @Slf4j
 @RestControllerAdvice
@@ -33,9 +26,9 @@ public class ErrorHandler{
         return ex.getMessage();
     }
 
-//    @ExceptionHandler(Exception.class)
-//    @ResponseStatus(HttpStatus.BAD_REQUEST)
-//    public String handleException(Exception ex) {
-//        return "Internal error occurred";
-//    }
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handleException(Exception ex) {
+        return "Internal error occurred";
+    }
 }
